258e7a54c515466a24fc03ebaa142844f0472359
joel aviles <joelaviles413@gmail.com>
2024-09-14 21:33:19 -0600

    🎨 style(welcome page): add label for sessionId and config button

--- frontend/src/components/WelcomePage.jsx
+++ frontend/src/components/WelcomePage.jsx
@@ -4,6 +4,10 @@ import QRCode from 'react-qr-code'
 const WelcomePage = () => {
 	return (
 		<div className="min-h-screen bg-gradient-to-b from-pink-100 to-blue-100 flex flex-col items-center justify-center p-4">
+			<section className="absolute top-4 right-4">
+				<span className='text-base font-semibold'>Id de la sesión: </span>
+				<span className='italic'>123-456</span>
+			</section>
       <main className="bg-white rounded-lg shadow-xl p-8 max-w-md w-full text-center">
         <h1 className="text-3xl font-bold mb-4 text-gray-800">¿Es niño ó niña?</h1>
         <p className="text-lg mb-6 text-gray-600">Bienvenido a este momento especial! Escanea el código QR o ingresa el código para jugar.</p>
@@ -20,20 +24,18 @@ const WelcomePage = () => {
 				<label htmlFor="gameCode" className="block font-semibold text-4xl text-gray-700 mb-6">
 					Código: 123456
 				</label>
-        <div className="mb-6">
-          <button className="w-full bg-black hover:bg-neutral-700 rounded-md text-white py-2 px-4 mb-2">
-						<Link to="/game" className='text-xl'>
-							Comenzar el juego
-						</Link>
-					</button>
 
-					<button className="w-full border border-black hover:bg-neutral-700 hover:text-white rounded-md text-black py-2 px-4">
-          <Link to="/config" className='text-lg'>
-            Configuración
-          </Link>
-        </button>
-        </div>
+				<button className="w-full bg-black hover:bg-neutral-700 rounded-md text-white py-2 px-4">
+					<Link to="/game" className='text-xl'>
+						Comenzar el juego
+					</Link>
+				</button>
       </main>
+			<button className="absolute top-4 left-4 border border-black hover:bg-neutral-700 hover:text-white rounded-md text-black py-2 px-4">
+				<Link to="/config" className='text-lg'>
+					⚙ Configuración
+				</Link>
+			</button>
     </div>
 	)
 }

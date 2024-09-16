571b1f56685cf58ddcc99b3d28baccbfe4fb1dd7
joel aviles <joelaviles413@gmail.com>
2024-09-16 15:25:47 -0600

    ✨ feat(frontend): add sound effects to pop ballon

--- frontend/src/components/GamePage.jsx
+++ frontend/src/components/GamePage.jsx
@@ -1,11 +1,16 @@
-import { motion } from "framer-motion";
-import { useState, useEffect, useRef } from "react";
-import Ballon from "./Ballon";
+import { motion } from 'framer-motion';
+import { useState, useEffect, useRef } from 'react';
+import useSound from 'use-sound';
 import JSConfetti from 'js-confetti';
+import Ballon from './Ballon';
+
+import popSound from '../assets/audio/pop-sound.mp3'
 
 const GamePage = () => {
   const [clicks, setClicks] = useState(0);
-  const ballonLife = 10;
+	const [play] = useSound(popSound)
+
+  const ballonLife = 5;
   const progress = (clicks / ballonLife) * 100;
 
   const jsConfetti = useRef(null);
@@ -18,24 +23,26 @@ const GamePage = () => {
     if (clicks < ballonLife) {
       setClicks(prev => {
         const newClicks = prev + 1;
+
         if (newClicks >= ballonLife) {
+					play()
           jsConfetti.current.addConfetti();
         }
+
         return newClicks;
       });
     }
   };
 
   return (
-    <div className="flex flex-col gap-12 items-center justify-center h-screen bg-gray-100 m-0 p-0 overflow-hidden">
-      <div className="absolute top-8">
-        <h1 className="text-3xl font-bold mb-4">¡Haz clic en el globo para reventarlo!</h1>
-				<span>{progress}</span>
+    <main className='flex flex-col gap-12 items-center justify-center h-screen bg-gray-100 m-0 p-0 overflow-hidden'>
+      <div className='absolute top-8'>
+        <h1 className='text-3xl font-bold mb-4'>¡Haz clic en el globo para reventarlo!</h1>
 
-        <div className="w-full mt-6">
-          <div className="w-full bg-gray-300 rounded-full h-6">
+        <div className='w-full mt-6'>
+          <div className='w-full bg-gray-300 rounded-full h-6'>
             <div
-              className={`h-6 rounded-full ${progress >= 100 ? "bg-red-500" : progress >= 70 ? "bg-orange-500" : "bg-green-500"}`}
+              className={`h-6 rounded-full ${progress >= 100 ? 'bg-red-500' : progress >= 70 ? 'bg-orange-500' : 'bg-green-500'}`}
               style={{ width: `${Math.min(progress, 100)}%` }}
             ></div>
           </div>
@@ -47,7 +54,7 @@ const GamePage = () => {
 				? (<span>Trono el globo!!!</span>)
 				: (
 					<motion.div
-						className="w-48 h-48 flex items-center justify-center cursor-pointer"
+						className='w-48 h-48 flex items-center justify-center cursor-pointer'
 						animate={{ scale: 1 + clicks * 0.05 }}
 						onClick={handleClick}
 					>
@@ -56,7 +63,7 @@ const GamePage = () => {
 				) 
 			}
       
-    </div>
+    </main>
   );
 };
 

8f39c3caa7f6b513bcab7e3d99e36c5f21a6bd3a
joel aviles <joelaviles413@gmail.com>
2024-09-16 19:45:02 -0600

    ✨ feat(frontend): implement zustand store

--- frontend/src/components/WelcomePage.jsx
+++ frontend/src/components/WelcomePage.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom'
 import QRCode from 'react-qr-code'
 
+import useGameStore from '../store/useGameStore'
+
 const WelcomePage = () => {
+	const sessionID = useGameStore((state) => state.sessionID);
+	const sessionCode = useGameStore((state) => state.sessionCode)
+
 	return (
 		<div className="min-h-screen bg-gradient-to-b from-pink-100 to-blue-100 flex flex-col items-center justify-center p-4">
 			<section className="absolute top-4 right-4">
 				<span className='text-base font-semibold'>Id de la sesión: </span>
-				<span className='italic'>123-456</span>
+				<span className='italic'>{sessionID}</span>
 			</section>
       <main className="bg-white rounded-lg shadow-xl p-8 max-w-md w-full text-center">
         <h1 className="text-3xl font-bold mb-4 text-gray-800">¿Es niño ó niña?</h1>
@@ -16,13 +21,13 @@ const WelcomePage = () => {
 				<QRCode
 					size={256}
 					style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-					value={'http://192.168.0.104:5173/player'}
+					value={`http://192.168.0.104:5173/player?sessionId=${sessionID}`}
 					viewBox={`0 0 256 256`}
 				/>
         </div>
         
 				<label htmlFor="gameCode" className="block font-semibold text-4xl text-gray-700 mb-6">
-					Código: 123456
+					Código: {sessionCode}
 				</label>
 
 				<button className="w-full bg-black hover:bg-neutral-700 rounded-md text-white py-2 px-4">
